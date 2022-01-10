f58cfb63ac86147e21f05c9568863ed4ec003cde
nikolomouri <niko.lomouri.1@btu.edu.ge>
2022-01-10 20:17:22 +0400

    Begining

--- app/src/main/java/com/example/mobappsfinallydone/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/mobappsfinallydone/fragments/LoginFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.mobappsfinallydone.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment:Fragment(R.layout.login_fragment) {
     private lateinit var eTEmail:EditText
@@ -13,12 +16,43 @@ class LoginFragment:Fragment(R.layout.login_fragment) {
     private lateinit var loginButton:Button
     private lateinit var forgetPasswordButton:Button
     private lateinit var registrationButton:Button
+    private lateinit var mAuth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
         eTEmail = view.findViewById(R.id.eTEmail)
         eTPassword = view.findViewById(R.id.eTPassword)
         loginButton = view.findViewById(R.id.loginButton)
         forgetPasswordButton = view.findViewById(R.id.forgetPasswordButton)
         registrationButton = view.findViewById(R.id.registrationButton)
+        val navController = Navigation.findNavController(view)
+        registrationButton.setOnClickListener {
+            val toRegistration = LoginFragmentDirections.actionLoginFragmentToRegistrationFragment()
+            Navigation.findNavController(view).navigate(toRegistration)
+        }
+        forgetPasswordButton.setOnClickListener {
+            val toForgetPassword = LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment()
+            Navigation.findNavController(view).navigate(toForgetPassword)
+        }
+        loginButton.setOnClickListener {
+            val email = eTEmail.text.toString()
+            val password = eTPassword.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this.context, "Email and pasword is empty",Toast.LENGTH_SHORT).show()
+            }else {
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            val action = 1
+                            val visible = LoginFragmentDirections.actionLoginFragmentToProfileFragment(action)
+                            navController.navigate(visible)
+                        }else {
+                            Toast.makeText(this.context, "incorect email or password" , Toast.LENGTH_SHORT).show()
+                        }
+                }
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mobappsfinallydone/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/mobappsfinallydone/fragments/ProfileFragment.kt
@@ -2,12 +2,12 @@ package com.example.mobappsfinallydone.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
 import com.example.mobappsfinallydone.R
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment:Fragment(R.layout.profile_fragment) {
     private lateinit var profileIV:ImageView
@@ -17,6 +17,8 @@ class ProfileFragment:Fragment(R.layout.profile_fragment) {
     private lateinit var loginButton2:Button
     private lateinit var changeProfileImageButton:Button
     private lateinit var changePassword:Button
+    private lateinit var layout1:LinearLayout
+    private lateinit var mAuth: FirebaseAuth
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         profileIV = view.findViewById(R.id.profileIV)
@@ -25,6 +27,34 @@ class ProfileFragment:Fragment(R.layout.profile_fragment) {
         tVSecondName = view.findViewById(R.id.tVSecondName)
         changeProfileImageButton = view.findViewById(R.id.changeProfileImageButton)
         changePassword = view.findViewById(R.id.changePassword)
+        layout1 = view.findViewById(R.id.layout1)
+        mAuth = FirebaseAuth.getInstance()
+        val navController = Navigation.findNavController(view)
+//        val visible = ProfileFragmentArgs.fromBundle(requireArguments()).action
         loginButton2 = view.findViewById(R.id.loginButton2)
+        loginButton2.setOnClickListener {
+            val toLogin = ProfileFragmentDirections.actionProfileFragmentToLoginFragment()
+            navController.navigate(toLogin)
+        }
+        changePassword.setOnClickListener {
+            val toChangePassword = ProfileFragmentDirections.actionProfileFragmentToChangePasswordFragment()
+            navController.navigate(toChangePassword)
+        }
+        changeProfileImageButton.setOnClickListener {
+            if (eTPhoto.text.isEmpty()){
+                Toast.makeText(this.context ,"The field is empty, if u want to change photo put url in it." ,Toast.LENGTH_LONG).show()
+            }else {
+                Glide.with(this).load(eTPhoto.text).into(profileIV)
+            }
+        }
+//        if (){
+//            layout1.visibility = View.VISIBLE
+//            loginButton2.visibility = View.GONE
+//        }
+//        else{
+//            layout1.visibility = View.INVISIBLE
+//            loginButton2.visibility = View.VISIBLE
+//
+//        }
     }
 }
